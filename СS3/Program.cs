8e7bd46654722e1259875c63e61b51e3376b81c0
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-12 17:44:17 +0300

    Добавьте файлы проекта.

---
+++ СS3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace СS3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            int age;
+            String color;
+            String fear;
+            String drink;
+            Console.Write("Как вас зовут? ");
+            name = Console.ReadLine();
+            Console.Write("Сколько вам лет? ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ваш любимый цвет? ");
+            color = Console.ReadLine();
+            Console.Write("Чего вы боитесь? ");
+            fear = Console.ReadLine();
+            Console.Write("Ваш любимый напиток? ");
+            drink = Console.ReadLine();
+            Console.WriteLine($"Вас зовут - {name}. Вам - {age} лет. Ваш любимый цвет - {color}. " +
+                $"Вы боитесь - {fear}. Ваш любимый напиток - {drink}.");
+        }
+    }
+}
